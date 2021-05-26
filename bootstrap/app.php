f8e6a03795a341d431e63c95d68486e7d81f6f2b
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-05-26 19:55:09 +0700

    enable cors for vue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -75,6 +75,9 @@ $app->configure('app');
 // $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
 // ]);
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
@@ -93,6 +96,7 @@ $app->routeMiddleware([
 
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\CatchAllOptionsRequestsProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
