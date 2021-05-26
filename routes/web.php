46a30821d0946daf4dcd0b86c552eb7e156a013c
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-05-26 19:54:45 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ $router->group(['prefix' => 'posts', 'namespace' => 'Admin'], function () use ($
     $router->get('/', 'PostController@index');
     $router->post('/store', 'PostController@store');
     $router->get('/show/{id}', 'PostController@show');
+    $router->patch('/update/{id}', 'PostController@update');
+    $router->delete('/delete/{id}', 'PostController@destroy');
 });
